b77b8f997930602b60d5d277473cffb597943332
Prakash Kafle <kafleprakash96@gmail.com>
2024-03-05 09:27:56 -0600

    Initial Commit

---
+++ src/main/java/com/ingeniorx/butterscotch/java/upstreamconsumer/UpstreamConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.ingeniorx.butterscotch.java.upstreamconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpstreamConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpstreamConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ingeniorx/butterscotch/java/upstreamconsumer/UpstreamConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingeniorx.butterscotch.java.upstreamconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpstreamConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
